319357628e6a9447946f09350775abe81123c385
drnachio <nacho@binpar.com>
2020-09-15 19:58:38 +0200

    fix

--- pages/healthcheck.tsx
+++ pages/healthcheck.tsx
@@ -1,8 +1,6 @@
 import { GetServerSideProps } from 'next';
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  // eslint-disable-next-line no-console
-  console.log('Health Check...');
   context.res.end('OK');
   return {
     props: {
